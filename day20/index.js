5530b574a7bdb4951ddaffe32f10265c321f8787
Ashish6312 <ashishsharma120512@gmail.com>
2025-09-05 00:12:09 +0530

    update

---
+++ day20/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from 'D:\Samadhan 2.0\Samadhan-2.0\day20\App.js';
+import './App.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
